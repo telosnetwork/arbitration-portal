acb72b85d3d71f05168024db15ea667f686677e0
ignatiusrevi <ignatiusrevi38@gmail.com>
2019-02-05 01:43:52 +1100

    added scatter and io client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './resources/logo.svg';
 import './styles/App.css';
+import ScatterBridge        from './utils/scatterBridge';
+import IOClient             from './utils/io-client';
+import Button from '@material-ui/core/Button'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    // state = {
+
+    // }
+
+    constructor (props) {
+        super(props);
+        this.appName = 'Telos-Portal';
+        this.network = {
+          blockchain: `eos`,
+          protocol:   `https`,
+          host:       `api.kylin.alohaeos.com`,
+          port:       443,
+          chainId:    `5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191`
+        };
+        console.log(this.network);
+        this.eosio = new ScatterBridge(this.network, this.appName);
+        // this.io    = new IOClient();
+    }
+
+    transfer = async () => {
+        let actions = await this.eosio.makeAction('eosio.token', 'transfer', 
+            {
+              from:      this.eosio.currentAccount.name,
+              to:       'emanateissue',
+              quantity: '1.0000 EOS',
+              memo:     'Transfer Memo'
+            }
+        );
+        let result = await this.eosio.sendTx(actions);
+        console.log('Results: ', result);
+    }
+
+    handleLogin = async () => {
+        await this.eosio.connect();
+        await this.eosio.login();
+    }
+
+    logout = async () => {
+        await this.eosio.logout();
+    }
+
+    render() {
+        document.title='Telos Portal'
+          return (
+              <div className='App'>
+                  <div className='BtnDiv'>
+                      <div className='Btn'>
+                          <Button variant='contained' color='primary' onClick={this.handleLogin.bind(this)}>LOGIN</Button>
+                      </div>
+                      <div className='Btn'>
+                          <Button variant='contained' color="primary" onClick={this.transfer.bind(this)}>TRANSFER</Button>
+                      </div>
+                      <div className='Btn'>
+                          <Button variant='contained' color="primary" onClick={this.logout.bind(this)}>LOGOUT</Button>
+                      </div>
+                  </div>
+              </div>
+          );
+      }
   }
-}
 
 export default App;
---
+++ frontend/src/utils/scatterBridge.js
@@ -0,0 +1,90 @@
+import ScatterJS        from "scatterjs-core";
+import ScatterEOS       from "scatterjs-plugin-eosjs2";
+import { Api, JsonRpc } from "eosjs";
+
+export default class ScatterBridge {
+    
+    constructor(network, appName) {
+
+        this.appName = appName; 
+
+        // Instantiate ScatterJS and the selected Plugins
+        ScatterJS.plugins(new ScatterEOS());
+
+        this.scatter = ScatterJS.scatter;
+
+        // Network Configuration to Connect (Reference) to Blockchain Endpoint Node
+        /**
+         * const network = ScatterJS.Network.fromJson({
+         *      blockchain: process.env.BLOCKCHAIN,
+         *      chainId:    process.env.CHAIN_ID,
+         *      host:       process.env.HOST,
+         *      port:       process.env.PORT,
+         *      protocol:   process.env.PROTOCOL
+         * });
+         */
+        this.network = network;
+
+        // Blockchain Wrapper to Wrap the actual Blockchain Libraries (eosjs)
+        /**
+         * const rpc = new JsonRpc(network.fullhost());
+         */
+        const rpc = new JsonRpc(`${network.protocol}://${network.host}:${network.port}`);
+
+        this.rpc = rpc;
+        this.currentAccount = null;
+        this.reqFields = { accounts: [network] };
+
+        // Signature (PROXY) Provider
+        /**
+         * const eos = ScatterJS.eos(network, Api, {rpc, beta3:true});
+         */
+        this.api = this.scatter.eos(network, Api, {rpc, beta3: true});
+
+        this.isConnected = false;
+        
+        window.ScatterJS = null;
+        window.ScatterEOS = null;
+
+    }
+
+    connect = async () => {
+        this.isConnected = await this.scatter.connect(this.appName);
+    }
+
+    login = async () => {
+        if (this.isConnected) {
+            await this.scatter.login(this.reqFields);
+            this.currentAccount = this.scatter.identity.accounts.find(x => x.blockchain === `eos`);
+            alert('Login Successful to Scatter');
+        } else {
+            alert('Login Fail - Please Login to Scatter');
+        }
+    }
+
+    logout = async () => {
+        this.scatter.logout();
+        alert('Logout Successful');
+    }
+
+    sendTx = async (actions) => {
+        if ([actions].length) {
+            try {
+                return await this.api.transact({
+                    actions: [actions]
+                }, { blocksBehind: 3, expireSeconds: 30 });
+            } catch (e) {
+                console.error('Trx Error: ', e);
+            }
+        } else console.log('Trx Error');
+    }
+
+    makeAction = async (contract, actionName, data, perm = { actor: this.currentAccount.name, permission: this.currentAccount.authority }) => {
+        return {
+            account: contract,
+            name: actionName,
+            authorization: [perm],
+            data: data
+        };
+    }
+}
